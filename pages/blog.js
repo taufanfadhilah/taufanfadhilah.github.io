626dc8d4ab2a02d7180e81e36bba3f1cbababe58
taufan.tanijoy <taufan@tanijoy.id>
2019-10-10 10:08:36 +0700

    create projects and blog page

---
+++ pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layouts from '../components/Layouts'
+
+function Blog(){
+    return (
+        <Layouts>
+            <h1>Blog</h1>
+        </Layouts>
+    )
+}
+
+export default Blog
