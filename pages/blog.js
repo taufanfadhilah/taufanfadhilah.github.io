fec34d181cb415d6b7accd0624b017a2192fcffb
taufanfadhilah <taufan@tanijoy.id>
2019-10-14 15:34:54 +0700

    paginate

--- pages/blog.js
+++ pages/blog.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Layouts from "../components/Layouts";
 import Card from "../components/Card";
 import { useSelector } from "react-redux";
 
 function Blog() {
   const blogs = useSelector(state => state.blogs);
+  const [page, setPage] = useState(0);
 
   return (
     <Layouts>
-      <div className="row">
+      <div className="row" id="blog">
         <div
           className="col"
           style={{ paddingBottom: 20, borderBottom: "1px solid #ffffff" }}
@@ -17,9 +18,27 @@ function Blog() {
         </div>
       </div>
       <div className="row mt-3 p-3">
-        {blogs.map((blog, index) => (
+        {blogs.slice(page * 6, 6 + page * 6).map((blog, index) => (
           <Card key={index} blog={blog} />
         ))}
+      </div>
+      <div className="row text-center">
+        <div className="col">
+          <p>
+            <span
+              className="cursor"
+              onClick={() => setPage(page - 1)}
+              hidden={page == 0}
+            >
+              Prev
+            </span>{" "}
+            <a href="#blog">
+              <span className="cursor" onClick={() => setPage(page + 1)}>
+                Next
+              </span>
+            </a>
+          </p>
+        </div>
       </div>
     </Layouts>
   );
